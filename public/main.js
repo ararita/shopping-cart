c5db953664b89a8dca73abc6f566d81abd537c8c
ararita <munjizavesna@gmail.com>
2020-08-15 12:54:01 +0200

    Add localStorage setup for products

--- public/main.js
+++ public/main.js
@@ -10,10 +10,19 @@ var app = new Vue({
   },
   mounted() {
     var storedCart = localStorage.getItem("cart");
+    var storedProducts = localStorage.getItem("products");
+
     if (storedCart) {
       //truthy if not false, undefined, "", 0
       console.log("Found a stored cart and loading it now");
       this.cart = JSON.parse(storedCart);
+    }
+
+    if (storedProducts) {
+      console.log("Found stored products and loading it now");
+      this.products = JSON.parse(storedProducts);
+    } else {
+      localStorage.setItem("products", JSON.stringify(products));
     }
   },
   computed: {
@@ -22,22 +31,38 @@ var app = new Vue({
       let sum = 0;
       for (let i = 0; i < items.length; i++) {
         let item = items[i];
-        let price = item.price * item.quantity;
-        sum += price;
+        let itemsGroupPrice = item.price * item.quantity;
+        sum += itemsGroupPrice;
       }
       return sum;
+
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+      localStorage.setItem("products", JSON.stringify(this.products));
     },
-    calculateTotalPrice2() {
+    calculateProductGroupPrice() {
       let items = Object.values(this.cart.items);
-      // reduce((curr, acc) =>)
-      let totalSum = items.reduce((accumulatorSum, item) => {
-        let price = item.price * item.quantity;
-        return price + accumulatorSum;
-      }, 0);
-      console.log(items);
-      console.log(totalSum);
-      return totalSum;
+      let price = 0;
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        price = item.price * item.quantity;
+        console.log("------price", price);
+      }
+      console.log("after loop price", price);
+      return price;
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+      localStorage.setItem("products", JSON.stringify(this.products));
     },
+    // calculateTotalPrice2() {
+    //   let items = Object.values(this.cart.items);
+    //   // reduce((curr, acc) =>)
+    //   let totalSum = items.reduce((accumulatorSum, item) => {
+    //     let price = item.price * item.quantity;
+    //     return price + accumulatorSum;
+    //   }, 0);
+    //   console.log(items);
+    //   console.log(totalSum);
+    //   return totalSum;
+    // },
   },
   methods: {
     addToCart(product) {
@@ -45,15 +70,21 @@ var app = new Vue({
         let item = this.cart.items[product.id];
         if (item.stockCount > 0) {
           item.quantity++;
+          this.products[product.id].stockCount--;
+          // item.stockCount--;
+          this.cart.totalQuantity++;
         }
       } else {
         Vue.set(app.cart.items, product.id, product);
         Vue.set(app.cart.items[product.id], "quantity", 1);
+        this.cart.items[product.id].stockCount--;
+        this.cart.totalQuantity++;
       }
-      this.cart.totalQuantity++;
-      this.cart.items[product.id].stockCount--;
+      // this.cart.totalQuantity++;
+      // this.cart.items[product.id].stockCount--;
 
       localStorage.setItem("cart", JSON.stringify(this.cart));
+      localStorage.setItem("products", JSON.stringify(this.products));
     },
 
     removeQuantity(product) {
@@ -69,6 +100,7 @@ var app = new Vue({
         item.stockCount++;
       }
       localStorage.setItem("cart", JSON.stringify(this.cart));
+      localStorage.setItem("products", JSON.stringify(this.products));
     },
   },
 });
