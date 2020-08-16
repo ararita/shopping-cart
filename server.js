990cc25a587e3993974af2e712dc6ad74be4df8e
ararita <munjizavesna@gmail.com>
2020-08-16 14:58:56 +0200

    Add style to cart, create modal

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/cart", function (request, response) {
-  response.sendFile(__dirname + "/views/cart.html");
-});
+// app.get("/cart", function (request, response) {
+//   response.sendFile(__dirname + "/views/cart.html");
+// });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
